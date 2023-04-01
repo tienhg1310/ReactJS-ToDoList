f9c48bc3b3a43a0cb18a7da8e77ab275d40aef41
Tienhg2001 <tienhg201@gmail.com>
2023-04-02 02:02:26 +0700

    setup day 1

---
+++ src/Components/TodoWrapper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoForm from './TodoForm'
+
+const TodoWrapper = () => {
+  return (
+    <div>
+        hello
+        <TodoForm></TodoForm>
+    </div>
+  )
+}
+
+export default TodoWrapper
